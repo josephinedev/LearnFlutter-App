841c86323bd2e9981744a1be622f6b4e20ec7eee
josephinedev <josephine.mugisho@gmail.com>
2019-01-01 15:07:23 +0300

    Adding more color parameters

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,75 @@
 //import 'package:flutter/material.dart';
 
 //void main() {
-// runApp(new MyApp());
+// runApp(MyApp());
 //}
 
-//class MyApp extends StatelessWidget {
-//  const MyApp({
-//  Key key,
-//}) : super(key: key);
+//class MyApp extends StatefulWidget {
+//  @override
+//  State<StatefulWidget> createState() {
+//    return MyAppState();
+//  }
+//}
+
+//class MyAppState extends State<MyApp> {
+// bool isToggled = false;
 
 //@override
 //Widget build(BuildContext context) {
-//return MaterialApp(
-//title: "EL App",
-//     theme: ThemeData(primarySwatch: Colors.purple),
-//   home: Scaffold(
-//   appBar: AppBar(
-//   title: Text("Church App"),
-// centerTitle: true,
+// return MaterialApp(
+//  title: 'Flutter Demo',
+//  theme: ThemeData(
+//  primarySwatch: Colors.orange,
 //),
-//body: Container(
-// color: Colors.white,
-//child: Center(
-//child: Column(
-// mainAxisAlignment: MainAxisAlignment.center,
-//  children: <Widget>[
-//  buildButton(),
-//],
-//),
-//),
+//home: Scaffold(
+//appBar: AppBar(),
+//body: ListView(
+// children: [
+//   buildRow("Manchester United", 22),
+// buildRow("Juventus", 18),
+//buildRow("Real Madrid", 19),
+//buildRow("PSG", 17),
+//buildRow("Kabasha", 14),
+//buildRow("Mazembe", 22),
+//buildRow("Virunga", 13),
+//buildRow("DCMP", 16),
+//buildRow("Volcano", 13),
+// ],
 //),
 //),
 //);
 //}
 
-//RaisedButton buildButton() {
-// return RaisedButton(
-//   child: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-//   Icon(
-//   Icons.print,
-// color: Colors.purple,
+//Container buildRow(String team, int points) {
+//return Container(
+// decoration: BoxDecoration(
+//   border: Border.all(color: Colors.orange, width: 2),
+// borderRadius: BorderRadius.all(Radius.circular(4))),
+//margin: EdgeInsets.all(20),
+//padding: EdgeInsets.all(5),
+//child: Row(
+// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//children: [
+//Text(
+//"$team",
+//style: TextStyle(fontSize: 21),
 //),
-//]),
-//onPressed: () {
-// print("Some one clicked");
-//});
+//Container(
+// padding: EdgeInsets.all(3),
+//decoration: BoxDecoration(
+//  color: Colors.orange,
+//borderRadius: BorderRadius.all(Radius.circular(4))),
+//child: Text(
+//"$points",
+//style: TextStyle(fontSize: 21, color: Colors.white),
+//),
+//),
+//],
+//),
+//);
 //}
 //}
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
   runApp(MyApp());
@@ -56,17 +78,17 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
+    // TODO: implement createState
     return MyAppState();
   }
 }
 
 class MyAppState extends State<MyApp> {
-  bool isToggled = false;
-
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Joe App',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
@@ -74,25 +96,19 @@ class MyAppState extends State<MyApp> {
         appBar: AppBar(),
         body: ListView(
           children: [
-            buildRow("Manchester United", 22),
-            buildRow("Juventus", 18),
-            buildRow("Real Madrid", 19),
-            buildRow("PSG", 17),
-            buildRow("Kabasha", 14),
-            buildRow("Mazembe", 22),
-            buildRow("Virunga", 13),
-            buildRow("DCMP", 16),
-            buildRow("Volcano", 13),
+            buildRow("Machester United", 20, Colors.green, Colors.red),
+            buildRow("Juventus", 18, Colors.orange, Colors.purple),
+            buildRow("Real Madrid", 19, Colors.blue, Colors.pink),
           ],
         ),
       ),
     );
   }
 
-  Container buildRow(String team, int points) {
+  Container buildRow(String team, int point, Color countorColor, Color numberColor) {
     return Container(
       decoration: BoxDecoration(
-          border: Border.all(color: Colors.orange, width: 2),
+          border: Border.all(color: countorColor, width: 2),
           borderRadius: BorderRadius.all(Radius.circular(4))),
       margin: EdgeInsets.all(20),
       padding: EdgeInsets.all(5),
@@ -106,10 +122,10 @@ class MyAppState extends State<MyApp> {
           Container(
             padding: EdgeInsets.all(3),
             decoration: BoxDecoration(
-                color: Colors.orange,
+                color: numberColor,
                 borderRadius: BorderRadius.all(Radius.circular(4))),
             child: Text(
-              "$points",
+              "$point",
               style: TextStyle(fontSize: 21, color: Colors.white),
             ),
           ),
